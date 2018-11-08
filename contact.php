7ab06d07c749a57b808202cf5ffb3fd7e6510209
egarcia-10 <44822988+egarcia-10@users.noreply.github.com>
2018-11-07 21:26:44 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+<title>Home</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="floater.css">
+<style>
+	.iframe {text-align:center}
+</style>
+
+</head>
+
+<body>
+	<div class="wrapper">
+
+		<header><h1>Double Play Baseball Facility</h1></header>
+		
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="instruct.php">Instructors</a></li>
+				<li><a href="merch.php">Merchandise</a></li>
+			</ul>
+		</nav>
+		<div class="rightco">
+			<main>
+				<h2 style = "text-align:center"> Contact!</h2>
+				
+				<p>At Double Play Hitting we look forward to serving you whenever
+				you're in need of practice, instruction, or just some new batting gloves. Please check out 
+				our <a href="https://twitter.com/" target="_blank">Twitter</a> page and like us on 
+				<a href="https://www.facebook.com/" target="_blank">Facebook</a>. We strive to make our facilities 
+				a better place, so please contact us with any concern, or praise, if needed.</p>
+
+				<p>Address:<br>
+				5555 Way Back Way<br>
+				Big, TX<br>
+				55344</p>
+
+				<p>Phone Number:<br>
+				345-345-3456</p>
+
+				<p>Email:<br>
+				dphitting@cages.com</p>
+
+			</main>
+		</div>
+	</div>
+<footer>Copyright &copy Eric Garcia</footer>
+</body>
+
+</html>
